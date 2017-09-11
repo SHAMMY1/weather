be017e5c1bd59d6362915e94beed53e16d13fb39
SHAMMY <Shammy.bg1@gmail.com>
2017-09-11 06:10:55 +0300

    change \\ /

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const { CommonsChunkPlugin } = require('webpack').optimize;
 const { AotPlugin } = require('@ngtools/webpack');
 
 const nodeModules = path.join(process.cwd(), 'node_modules');
-const entryPoints = ["inline","polyfills","sw-register","styles","vendor","main"];
+const entryPoints = ["inline", "polyfills", "sw-register", "styles", "vendor", "main"];
 const baseHref = "";
 const deployUrl = "";
 
@@ -36,11 +36,11 @@ module.exports = {
   },
   "entry": {
     "main": [
-      "./src\\main.ts"
+      "./src/main.ts"
     ],
     "styles": [
-      "./src\\styles.css",
-      "./node_modules\\bootstrap\\dist\\css\\bootstrap.min.css"
+      "./src/styles.css",
+      "./node_modules/bootstrap/dist/css/bootstrap.min.css"
     ]
   },
   "output": {
@@ -76,8 +76,8 @@ module.exports = {
       },
       {
         "exclude": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.css$/,
         "loaders": [
@@ -88,8 +88,8 @@ module.exports = {
       },
       {
         "exclude": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.scss$|\.sass$/,
         "loaders": [
@@ -101,8 +101,8 @@ module.exports = {
       },
       {
         "exclude": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.less$/,
         "loaders": [
@@ -114,8 +114,8 @@ module.exports = {
       },
       {
         "exclude": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.styl$/,
         "loaders": [
@@ -127,66 +127,66 @@ module.exports = {
       },
       {
         "include": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.css$/,
         "loaders": ExtractTextPlugin.extract({
-  "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader"
-  ],
-  "fallback": "style-loader",
-  "publicPath": ""
-})
+          "use": [
+            "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
+            "postcss-loader"
+          ],
+          "fallback": "style-loader",
+          "publicPath": ""
+        })
       },
       {
         "include": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.scss$|\.sass$/,
         "loaders": ExtractTextPlugin.extract({
-  "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "sass-loader"
-  ],
-  "fallback": "style-loader",
-  "publicPath": ""
-})
+          "use": [
+            "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
+            "postcss-loader",
+            "sass-loader"
+          ],
+          "fallback": "style-loader",
+          "publicPath": ""
+        })
       },
       {
         "include": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.less$/,
         "loaders": ExtractTextPlugin.extract({
-  "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "less-loader"
-  ],
-  "fallback": "style-loader",
-  "publicPath": ""
-})
+          "use": [
+            "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
+            "postcss-loader",
+            "less-loader"
+          ],
+          "fallback": "style-loader",
+          "publicPath": ""
+        })
       },
       {
         "include": [
-          path.join(process.cwd(), "src\\styles.css"),
-          path.join(process.cwd(), "node_modules\\bootstrap\\dist\\css\\bootstrap.min.css")
+          path.join(process.cwd(), "src/styles.css"),
+          path.join(process.cwd(), "node_modules/bootstrap/dist/css/bootstrap.min.css")
         ],
         "test": /\.styl$/,
         "loaders": ExtractTextPlugin.extract({
-  "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
-  ],
-  "fallback": "style-loader",
-  "publicPath": ""
-})
+          "use": [
+            "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
+            "postcss-loader",
+            "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
+          ],
+          "fallback": "style-loader",
+          "publicPath": ""
+        })
       },
       {
         "test": /\.ts$/,
@@ -202,14 +202,14 @@ module.exports = {
         "favicon.ico"
       ],
       "globOptions": {
-        "cwd": "C:\\Users\\SHAMMY\\Desktop\\angular-teamwork\\weather\\src",
+        "cwd": "C:/Users/SHAMMY/Desktop/angular-teamwork/weather/src",
         "dot": true,
         "ignore": "**/.gitkeep"
       }
     }),
     new ProgressPlugin(),
     new HtmlWebpackPlugin({
-      "template": "./src\\index.html",
+      "template": "./src/index.html",
       "filename": "./index.html",
       "hash": false,
       "inject": true,
@@ -226,15 +226,15 @@ module.exports = {
         let leftIndex = entryPoints.indexOf(left.names[0]);
         let rightindex = entryPoints.indexOf(right.names[0]);
         if (leftIndex > rightindex) {
-            return 1;
+          return 1;
         }
         else if (leftIndex < rightindex) {
-            return -1;
+          return -1;
         }
         else {
-            return 0;
+          return 0;
         }
-    }
+      }
     }),
     new BaseHrefWebpackPlugin({}),
     new CommonsChunkPlugin({
@@ -257,26 +257,28 @@ module.exports = {
       "options": {
         "postcss": [
           autoprefixer(),
-          postcssUrl({"url": (URL) => {
-            // Only convert root relative URLs, which CSS-Loader won't process into require().
-            if (!URL.startsWith('/') || URL.startsWith('//')) {
+          postcssUrl({
+            "url": (URL) => {
+              // Only convert root relative URLs, which CSS-Loader won't process into require().
+              if (!URL.startsWith('/') || URL.startsWith('//')) {
                 return URL;
-            }
-            if (deployUrl.match(/:\/\//)) {
+              }
+              if (deployUrl.match(/:\/\//)) {
                 // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
                 return `${deployUrl.replace(/\/$/, '')}${URL}`;
-            }
-            else if (baseHref.match(/:\/\//)) {
+              }
+              else if (baseHref.match(/:\/\//)) {
                 // If baseHref contains a scheme, include it as is.
                 return baseHref.replace(/\/$/, '') +
-                    `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-            }
-            else {
+                  `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+              }
+              else {
                 // Join together base-href, deploy-url and the original URL.
                 // Also dedupe multiple slashes into single ones.
                 return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+              }
             }
-        }})
+          })
         ],
         "sassLoader": {
           "sourceMap": false,
@@ -291,10 +293,10 @@ module.exports = {
     new AotPlugin({
       "mainPath": "main.ts",
       "hostReplacementPaths": {
-        "environments\\environment.ts": "environments\\environment.ts"
+        "environments/environment.ts": "environments/environment.ts"
       },
       "exclude": [],
-      "tsConfigPath": "src\\tsconfig.json",
+      "tsConfigPath": "src/tsconfig.json",
       "skipCodeGeneration": true
     })
   ],
